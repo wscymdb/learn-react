cfcfae316d8c069c708f8481e54e0f5d8140ebbd
wscymdb <1724215773@qq.com>
2025-02-09 17:37:52 +0800

    feat: 合成事件的原理1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Demo1 from './pages/Demo1/index.tsx'
+import './index.less'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Demo1 />)
--- src/pages/Demo1/index.tsx
+++ src/pages/Demo1/index.tsx
@@ -1,10 +1,51 @@
-import React, { memo } from 'react'
-import './index.css'
+import React, { memo, useEffect } from 'react'
+
+const generFn = (msg, needE = false) => {
+  return (e) => {
+    console.log(msg)
+    needE && console.log(e.composedPath())
+  }
+}
 
 const Demo1 = memo(() => {
+  useEffect(() => {
+    const documentCaptureFn = generFn('document 捕获')
+    const documentFn = generFn('document 冒泡')
+    const bodyCaptureFn = generFn('body 捕获')
+    const bodyFn = generFn('body 冒泡')
+    const rootCaptureFn = generFn('root 捕获', true)
+    const rootFn = generFn('root 冒泡')
+    const rootEl = document.getElementById('root')
+
+    document.addEventListener('click', documentCaptureFn, true)
+    document.addEventListener('click', documentFn, false)
+
+    document.body.addEventListener('click', bodyCaptureFn, true)
+    document.body.addEventListener('click', bodyFn, false)
+
+    rootEl?.addEventListener('click', rootCaptureFn, true)
+    rootEl?.addEventListener('click', rootFn, false)
+  }, [])
+
   return (
-    <div className="outer">
-      <div className="inner">11</div>
+    <div
+      className="outer"
+      onClick={(e) => {
+        console.log('outer 冒泡')
+      }}
+      onClickCapture={() => {
+        console.log('outer 捕获')
+      }}
+    >
+      <div
+        className="inner"
+        onClick={() => {
+          console.log('inner 冒泡')
+        }}
+        onClickCapture={() => {
+          console.log('inner 捕获')
+        }}
+      ></div>
     </div>
   )
 })
