6c00896ea9ca40917fdd3a666835de5a305aaf83
cym01875771@alibaba-inc.com <cym01875771@alibaba-inc.com>
2025-02-11 19:24:12 +0800

    feat: 完善readme

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import React, { createElement } from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import './jsxHandler'
-import { render } from './jsxHandler'
+import React, { createElement } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./jsxHandler";
+import { render } from "./jsxHandler";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<App />)
 
+// 生成虚拟DOM
+// 这是babel转换jsx后的代码
 const ele = /*#__PURE__*/ React.createElement(
-  'div',
+  "div",
   {
-    id: 'a',
+    id: "a",
     style: {
-      color: 'red',
+      color: "red",
     },
   },
-  /*#__PURE__*/ React.createElement('span', null, 'hello'),
+  /*#__PURE__*/ React.createElement("span", null, "hello"),
   /*#__PURE__*/ React.createElement(
-    'span',
+    "span",
     {
-      class: 'abc',
+      class: "abc",
     },
-    'hi'
+    "hi"
   ),
-  /*#__PURE__*/ React.createElement('span', null, '\u4F60\u597D')
-)
+  /*#__PURE__*/ React.createElement("span", null, "\u4F60\u597D")
+);
 
 // console.log(ele)
 
-render(ele, document.getElementById('root'))
+// 自己实现render函数
+render(ele, document.getElementById("root"));
