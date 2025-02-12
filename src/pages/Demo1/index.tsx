2bd6cd1c835edfbb99c0a49e0a6a982c7a5b389a
puta <puta.com>
2025-02-12 17:54:17 +0800

    feat: 添加文档

--- src/pages/Demo1/index.tsx
+++ src/pages/Demo1/index.tsx
@@ -1,53 +1,67 @@
-import React, { memo, useEffect } from 'react'
-
-const generFn = (msg, needE = false) => {
-  return (e) => {
-    console.log(msg)
-    needE && console.log(e.composedPath())
-  }
-}
+import React, { memo, useEffect } from "react";
 
 const Demo1 = memo(() => {
+  // 生成一个函数
+  const generFn = (msg) => {
+    return (e) => {
+      console.log(msg);
+    };
+  };
+
   useEffect(() => {
-    const documentCaptureFn = generFn('document 捕获')
-    const documentFn = generFn('document 冒泡')
-    const bodyCaptureFn = generFn('body 捕获')
-    const bodyFn = generFn('body 冒泡')
-    const rootCaptureFn = generFn('root 捕获', true)
-    const rootFn = generFn('root 冒泡')
-    const rootEl = document.getElementById('root')
+    const documentCaptureFn = generFn("document 捕获");
+    const documentFn = generFn("document 冒泡");
+    const bodyCaptureFn = generFn("body 捕获");
+    const bodyFn = generFn("body 冒泡");
+    const rootCaptureFn = generFn("root 捕获");
+    const rootFn = generFn("root 冒泡");
+    const outerCaptureFn = generFn("outer 捕获「原生」");
+    const outerFn = generFn("outer 冒泡「原生」");
+    const innerCaptureFn = generFn("inner 捕获「原生」");
+    const innerFn = generFn("inner 冒泡「原生」");
+    const rootEl = document.getElementById("root");
+    const outerEl = document.getElementById("outer");
+    const innerEl = document.getElementById("inner");
 
-    document.addEventListener('click', documentCaptureFn, true)
-    document.addEventListener('click', documentFn, false)
+    document.addEventListener("click", documentCaptureFn, true);
+    document.addEventListener("click", documentFn, false);
 
-    document.body.addEventListener('click', bodyCaptureFn, true)
-    document.body.addEventListener('click', bodyFn, false)
+    document.body.addEventListener("click", bodyCaptureFn, true);
+    document.body.addEventListener("click", bodyFn, false);
 
-    rootEl?.addEventListener('click', rootCaptureFn, true)
-    rootEl?.addEventListener('click', rootFn, false)
-  }, [])
+    rootEl?.addEventListener("click", rootCaptureFn, true);
+    rootEl?.addEventListener("click", rootFn, false);
+
+    outerEl?.addEventListener("click", outerCaptureFn, true);
+    outerEl?.addEventListener("click", outerFn, false);
+
+    innerEl?.addEventListener("click", innerCaptureFn, true);
+    innerEl?.addEventListener("click", innerFn, false);
+  }, []);
 
   return (
     <div
-      className="outer"
+      id="outer"
       onClick={(e) => {
-        console.log('outer 冒泡')
+        console.log("outer 冒泡「合成」");
       }}
       onClickCapture={() => {
-        console.log('outer 捕获')
+        console.log("outer 捕获「合成」");
       }}
     >
       <div
-        className="inner"
-        onClick={() => {
-          console.log('inner 冒泡')
+        id="inner"
+        onClick={(e) => {
+          // e.stopPropagation();
+          e.nativeEvent.stopImmediatePropagation();
+          console.log("inner 冒泡「合成」");
         }}
         onClickCapture={() => {
-          console.log('inner 捕获')
+          console.log("inner 捕获「合成」");
         }}
       ></div>
     </div>
-  )
-})
+  );
+});
 
-export default Demo1
+export default Demo1;
